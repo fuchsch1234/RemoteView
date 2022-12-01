3be63222e05482c0d8160b078636280b56384447
cf <christian+github@fuchsch.de>
2022-12-01 22:16:47 +0100

    feat: add first e2e test
    Test that title of page returned by backend is set to
    expected value.

---
+++ src/jvmE2eTests/kotlin/de/fuchsch/remoteview/extension/TitleTest.kt
@@ -0,0 +1,32 @@
+package de.fuchsch.remoteview.extension
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.WebDriver
+
+class TitleTest {
+
+    private lateinit var driver: WebDriver
+
+    @BeforeEach
+    fun setup() {
+        driver = WebDriverManager.firefoxdriver().create()
+    }
+
+    @AfterEach
+    fun teardown() {
+        driver.quit()
+    }
+
+    @Test
+    fun `title is set`() {
+        driver.get("http://127.0.0.1:8080")
+        assertThat(driver.title, containsString("Ktor"))
+
+    }
+
+}
