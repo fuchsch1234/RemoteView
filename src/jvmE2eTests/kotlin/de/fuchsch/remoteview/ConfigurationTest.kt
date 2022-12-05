7822c4dfbd7288ba777e0708d361c20fe0231dbc
cf <christian+github@fuchsch.de>
2022-12-05 22:07:23 +0100

    test: add first testcase for configuration endpoint

---
+++ src/jvmE2eTests/kotlin/de/fuchsch/remoteview/ConfigurationTest.kt
@@ -0,0 +1,31 @@
+package de.fuchsch.remoteview
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.WebDriver
+
+class ConfigurationTest {
+
+    private lateinit var driver: WebDriver
+
+    @BeforeEach
+    fun setup() {
+        driver = WebDriverManager.firefoxdriver().create()
+    }
+
+    @AfterEach
+    fun teardown() {
+        driver.quit()
+    }
+
+    @Test
+    fun `configuration returns configured value`() {
+        driver.get("http://127.0.0.1:8080/config.json")
+        MatcherAssert.assertThat(driver.pageSource, CoreMatchers.containsString("oidc-test"))
+    }
+
+}
