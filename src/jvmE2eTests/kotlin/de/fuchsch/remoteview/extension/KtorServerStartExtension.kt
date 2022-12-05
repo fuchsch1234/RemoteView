c9618872d3bb3a8ceb8f8c5519bf01c32742ef43
cf <christian+github@fuchsch.de>
2022-12-05 22:06:53 +0100

    feat: start e2e test server with configuration properties
    Allows injecting configuration properties into tests.

--- src/jvmE2eTests/kotlin/de/fuchsch/remoteview/extension/KtorServerStartExtension.kt
+++ src/jvmE2eTests/kotlin/de/fuchsch/remoteview/extension/KtorServerStartExtension.kt
@@ -3,6 +3,7 @@ package de.fuchsch.remoteview.extension
 import de.fuchsch.remoteview.module
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.config.*
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import kotlin.concurrent.thread
@@ -15,9 +16,16 @@ class KtorServerStartExtension: BeforeAllCallback {
         if (!started) {
             started = true
             thread {
-                embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-                    module()
-                }.start(wait = true)
+                embeddedServer(Netty, environment = applicationEngineEnvironment {
+                    connector {
+                        host = "127.0.0.1"
+                        port = 8080
+                    }
+                    module {
+                        module()
+                    }
+                    config = MapApplicationConfig("oidc.clientId" to "oidc-test")
+                }).start(wait = true)
             }
         }
     }
