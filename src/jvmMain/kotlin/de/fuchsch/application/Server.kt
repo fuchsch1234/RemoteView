62c344ff650e677bf94c5e0f886a4808c0c592b7
cf <christian+github@fuchsch.de>
2022-12-05 22:04:24 +0100

    feat: use EngineMain to start ktor server
    Configuration is loaded from application.conf file in resources folder. Configuration entries can be overwritten at startup using the -P flag.

--- src/jvmMain/kotlin/de/fuchsch/application/Server.kt
+++ src/jvmMain/kotlin/de/fuchsch/application/Server.kt
@@ -1,12 +1,3 @@
 package de.fuchsch.application
 
-import de.fuchsch.remoteview.module
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-
-
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        module()
-    }.start(wait = true)
-}
+fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
